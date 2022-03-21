cb2ba689550465baeee9c0b7adfb086c4b15cd7e
zedite <164612078+zedite@users.noreply.github.com>
2022-03-21 14:00:27 +0000

    Fix JS error

--- function/FunctionRegistry.java
+++ function/FunctionRegistry.java
@@ -20,4 +20,26 @@ import io.confluent.ksql.function.udaf.count.CountAggFunctionDeterminer;
 import io.confluent.ksql.function.udaf.max.MaxAggFunctionDeterminer;
 import io.confluent.ksql.function.udaf.min.MinAggFunctionDeterminer;
 import io.confluent.ksql.function.udaf.sum.SumAggFunctionDeterminer;
-import io.confluent.ksql
+import io.confluent.ksql.function.udf.datetime.StringToTimestamp;
+import io.confluent.ksql.function.udf.datetime.TimestampToString;
+import io.confluent.ksql.function.udf.json.JsonExtractStringKudf;
+import io.confluent.ksql.function.udf.math.AbsKudf;
+import io.confluent.ksql.function.udf.math.CeilKudf;
+import io.confluent.ksql.function.udf.math.FloorKudf;
+import io.confluent.ksql.function.udf.math.RandomKudf;
+import io.confluent.ksql.function.udf.math.RoundKudf;
+import io.confluent.ksql.function.udf.ml.AnomalyKudf;
+import io.confluent.ksql.function.udf.string.ConcatKudf;
+import io.confluent.ksql.function.udf.string.IfNullKudf;
+import io.confluent.ksql.function.udf.string.LCaseKudf;
+import io.confluent.ksql.function.udf.string.LenKudf;
+import io.confluent.ksql.function.udf.string.SubstringKudf;
+import io.confluent.ksql.function.udf.string.TrimKudf;
+import io.confluent.ksql.function.udf.string.UCaseKudf;
+import io.confluent.ksql.parser.tree.Expression;
+import io.confluent.ksql.util.ExpressionTypeManager;
+import io.confluent.ksql.util.KsqlException;
+import org.apache.kafka.connect.data.Schema;
+
+import java.util.ArrayList;
+import java.util.Array
