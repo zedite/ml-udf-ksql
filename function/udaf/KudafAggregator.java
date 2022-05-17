dbe150a1250f7f1f2a6424bcf2130fda46901118
zedite <164612078+zedite@users.noreply.github.com>
2022-05-17 06:03:15 +0000

    Update site.yml

--- function/udaf/KudafAggregator.java
+++ function/udaf/KudafAggregator.java
@@ -17,4 +17,35 @@
 package io.confluent.ksql.function.udaf;
 
 import io.confluent.ksql.function.KsqlAggregateFunction;
-import io.co
+import io.confluent.ksql.GenericRow;
+import io.confluent.ksql.function.UdafAggregator;
+
+import org.apache.kafka.streams.kstream.Merger;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class KudafAggregator implements UdafAggregator {
+
+  private Map<Integer, KsqlAggregateFunction> aggValToAggFunctionMap;
+  private Map<Integer, Integer> aggValToValColumnMap;
+
+  public KudafAggregator(Map<Integer, KsqlAggregateFunction> aggValToAggFunctionMap, Map<Integer,
+      Integer> aggValToValColumnMap) {
+    this.aggValToAggFunctionMap = aggValToAggFunctionMap;
+    this.aggValToValColumnMap = aggValToValColumnMap;
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public GenericRow apply(String s, GenericRow rowValue, GenericRow aggRowValue) {
+    aggValToValColumnMap.forEach((key, value) ->
+        aggRowValue.getColumns().set(key, rowValue.getColumns().get(value))
+    );
+
+    aggValToAggFunctionMap.forEach((key, value) ->
+        aggRowValue.getColumns().set(key,
+            value.aggregate(rowValue.getColumns().get(value.getArgIndexInValue()),
+                aggRowValue.getCol
