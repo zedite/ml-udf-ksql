f0bceb0482ad3fcc86c2ea0c692fa15a6782a066
zedite <164612078+zedite@users.noreply.github.com>
2022-08-09 12:37:17 +0000

    Auto-merge from mysql-trunk-bugfixing.

--- function/udaf/max/MaxAggFunctionDeterminer.java
+++ function/udaf/max/MaxAggFunctionDeterminer.java
@@ -7,4 +7,33 @@
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applica
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+package io.confluent.ksql.function.udaf.max;
+
+import org.apache.kafka.connect.data.Schema;
+
+import java.util.Arrays;
+import java.util.List;
+
+import io.confluent.ksql.function.KsqlAggFunctionDeterminer;
+import io.confluent.ksql.function.KsqlAggregateFunction;
+import io.confluent.ksql.util.KsqlException;
+
+public class MaxAggFunctionDeterminer extends KsqlAggFunctionDeterminer {
+
+  public MaxAggFunctionDeterminer() {
+    super("MAX", Arrays.asList(new DoubleMaxKudaf(-1), new LongMaxKudaf(-1)));
+  }
+
+  @Override
+  public KsqlAggregateFunction getProperAggregateFunction(List<Schema> argTypeList) {
+    // For now we only support aggregate functions with one arg.
+    for (KsqlAggregateFunction ksqlAggregateFunction : getAggregateFunctionList()) {
+      if (ksqlAggregateFunction.getArguments().get(0) == argTypeList.get(0)) {
+        return ks
