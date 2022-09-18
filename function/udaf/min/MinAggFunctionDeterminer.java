d49ba08c458b44dee118855c64f4555533c3fc50
zedite <164612078+zedite@users.noreply.github.com>
2022-09-18 07:04:58 +0000

    #1938: updated FuncLib block and stats block

--- function/udaf/min/MinAggFunctionDeterminer.java
+++ function/udaf/min/MinAggFunctionDeterminer.java
@@ -11,4 +11,35 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations und
+ * limitations under the License.
+ **/
+
+package io.confluent.ksql.function.udaf.min;
+
+import org.apache.kafka.connect.data.Schema;
+
+import java.util.Arrays;
+import java.util.List;
+
+import io.confluent.ksql.function.KsqlAggFunctionDeterminer;
+import io.confluent.ksql.function.KsqlAggregateFunction;
+import io.confluent.ksql.util.KsqlException;
+
+public class MinAggFunctionDeterminer extends KsqlAggFunctionDeterminer {
+
+  public MinAggFunctionDeterminer() {
+    super("MIN", Arrays.asList(new DoubleMinKudaf(-1), new LongMinKudaf(-1)));
+  }
+
+  @Override
+  public KsqlAggregateFunction getProperAggregateFunction(List<Schema> argTypeList) {
+    // For now we only support aggregate functions with one arg.
+    for (KsqlAggregateFunction ksqlAggregateFunction : getAggregateFunctionList()) {
+      if (ksqlAggregateFunction.getArguments().get(0) == argTypeList.get(0)) {
+        return ksqlAggregateFunction;
+      }
+    }
+    throw new KsqlException("No Max aggregate function with " + argTypeList.get(0) + " "
+                            + " argument type exists!");
+  }
+}
---
+++ function/udaf/sum/LongSumKudaf.java
@@ -0,0 +1,4 @@
+/**
+ * Copyright 2017 Confluent Inc.
+ *
+ * Licensed unde
