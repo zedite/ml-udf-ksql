645de9e69cfd4b2a992d5babeafbb305faedf3fb
zedite <164612078+zedite@users.noreply.github.com>
2024-04-24 18:55:18 +0000

    Bumping version to 0.0.3

--- function/udf/string/LenKudf.java
+++ function/udf/string/LenKudf.java
@@ -1,3 +1,4 @@
+
 /**
  * Copyright 2017 Confluent Inc.
  *
@@ -19,7 +20,7 @@ package io.confluent.ksql.function.udf.string;
 import io.confluent.ksql.function.KsqlFunctionException;
 import io.confluent.ksql.function.udf.Kudf;
 
-public class LCaseKudf implements Kudf {
+public class LenKudf implements Kudf {
 
   @Override
   public void init() {
@@ -29,8 +30,8 @@ public class LCaseKudf implements Kudf {
   @Override
   public Object evaluate(Object... args) {
     if (args.length != 1) {
-      throw new KsqlFunctionException("LCase udf should have one input argument.");
+      throw new KsqlFunctionException("Length udf should have one input argument.");
     }
-    return args[0].toString().toLowerCase();
+    return args[0].toString().length();
   }
 }
